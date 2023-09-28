811049cdb78781598fbbb02c94ad7a4f017952f8
Valentin <valentinvalentinov027@gmail.com>
2023-09-28 18:37:23 +0300

    Configure handlebars view engine

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ const express = require('express');
 const { PORT } = require('./constants/constants');
 
 const { expressConfig } = require('./config/expressConfig');
+const { handlebarsConfig } = require('./config/handlebarsConfig');
 
 const app = express();
 
 expressConfig(app);
+handlebarsConfig(app);
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
