b83467cc4879d09964c3a7af754ff18f899c486e
Valentin <valentinvalentinov027@gmail.com>
2023-09-28 18:44:44 +0300

    Create router

--- src/index.js
+++ src/index.js
@@ -6,10 +6,14 @@ const { dbConfig } = require('./config/dbConfig');
 const { expressConfig } = require('./config/expressConfig');
 const { handlebarsConfig } = require('./config/handlebarsConfig');
 
+const routes = require('./routes');
+
 const app = express();
 
 dbConfig();
 expressConfig(app);
 handlebarsConfig(app);
+
+app.use(routes);
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
