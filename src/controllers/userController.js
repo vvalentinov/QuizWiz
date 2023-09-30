d6407e7b5a2a8356c1a986276ad8017de9978956
Valentin <valentinvalentinov027@gmail.com>
2023-09-30 15:00:18 +0300

    Add register and login pages

---
+++ src/controllers/userController.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+});
+
+router.get('/register', (req, res) => {
+    res.render('users/register');
+});
+
+module.exports = router;
