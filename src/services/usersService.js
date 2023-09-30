8c3ddc777487f0c49882e66ae805d2f66862a0df
Valentin <valentinvalentinov027@gmail.com>
2023-09-30 15:13:53 +0300

    Add register function in user service

--- src/services/usersService.js
+++ src/services/usersService.js
@@ -1,1 +1,17 @@
 const User = require('../models/User');
+
+const { generateToken } = require('../utils/generateToken');
+
+exports.register = async (userData) => {
+    const user = await User.findOne({ username: userData.username });
+
+    if (user) {
+        throw new Error('User already exists!');
+    }
+
+    const createdUser = await User.create(userData);
+
+    const token = await generateToken(createdUser._id, createdUser.username);
+
+    return token;
+};
