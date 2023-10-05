abbfa73d3901272554f05f710c038e50d66d6b43
Valentin <valentinvalentinov027@gmail.com>
2023-10-05 19:15:30 +0300

    Add initial quiz model

---
+++ src/models/Quiz.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const quizSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    creator: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+});
+
+const Quiz = mongoose.model('Quiz', quizSchema);
+
+module.exports = Quiz;
