ce4d95ef3fe44bae722ab5b7ff935f3870f3a76f
Valentin <valentinvalentinov027@gmail.com>
2023-10-06 12:29:11 +0300

    Create quiz controller

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const userController = require('./controllers/userController');
 const homeController = require('./controllers/homeController');
+const quizController = require('./controllers/quizController');
 
 router.use('/users', userController);
+router.use('/quiz', quizController);
 router.use(homeController);
 
 module.exports = router;
