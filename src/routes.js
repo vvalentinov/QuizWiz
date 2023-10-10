e5edc7370af8127aced7a55f2c6d84924294733e
Valentin <valentinvalentinov027@gmail.com>
2023-10-10 17:50:36 +0300

    Create category controller

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,12 @@ const userController = require('./controllers/userController');
 const userSettingsController = require('./controllers/userSettingsController');
 const homeController = require('./controllers/homeController');
 const quizController = require('./controllers/quizController');
+const categoryController = require('./controllers/categoryController');
 
 router.use('/user', userController);
 router.use('/user/settings', userSettingsController);
 router.use('/quiz', quizController);
+router.use('/category', categoryController);
 router.use(homeController);
 router.use('*', (req, res) => {
     res.status(404).render('404');
